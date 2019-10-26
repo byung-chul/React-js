56d4eaf0f2bcac06672bdb29efd3dd5f0efc7188
Byungchul <byung-chul@nate.com>
2019-10-26 17:57:19 +0900

    React js Practice

---
+++ hello-react/src/App.js
@@ -0,0 +1,14 @@
+import React, {
+    Component
+} from 'react';
+import MyName from './MyName';
+
+class App extends Component {
+    render() {
+        return ( 
+            <MyName  name = "React"/ >
+        );
+    }
+}
+
+export default App;
---
+++ hello-react/src/MyName.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const MyName = ({ name }) => {
+    return(
+        <div>
+            Hello!!! MyName is {name}!!!!!.
+        </div>
+    );
+};
+
+export default MyName;
